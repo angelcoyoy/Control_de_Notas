8ee98def3740a15c681e312b153499c20e46e1a0
angel <angelcoyoy713@gmail.com>
2018-11-19 11:59:42 -0600

    Creacion de vista.

--- Cnotas/views.py
+++ Cnotas/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import GradoForm
+from Cnotas.models import Grado, Asignacion
 
-# Create your views here.
+def grado_nuevo(request):
+    if request.method == "POST":
+        formulario = GradoForm(request.POST)
+        if formulario.is_valid():
+            grado = Grado.objects.create(nombre=formulario.cleaned_data['nombre'], semestre = formulario.cleaned_data['semestre'], seccion = formulario.cleaned_data['seccion'], jornada = formulario.cleaned_data['jornada'], anio = formulario.cleaned_data['anio'])
+            for materia_id in request.POST.getlist('materias'):
+                asignacion= Asignacion(materia_id=materia_id, grado_id = grado.id)
+                asignacion.save()
+            messages.add_message(request, messages.SUCCESS, 'Grado Guardado Exitosamente')
+    else:
+        formulario = GradoForm()
+    return render(request, 'Cnotas/grado_editar.html', {'formulario': formulario})
